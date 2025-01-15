4d540d2628049049f531e6dabe84a93c35f176aa
Pedro Santilli <psantillidesouza@gmail.com>
2025-01-14 22:34:10 -0300

    projeto 0.1

--- weather app/ViewController.swift
+++ weather app/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private lazy var customView : UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupViews()
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    private func setupViews() {
+        view.backgroundColor = UIColor.red
+        setHierarchy()
+        setConstraints()
+    }
+    private func setHierarchy() {
+        view.addSubview(customView)
+    }
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            customView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 100),
+            customView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 50),
+            customView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -50),
+            customView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100)
+        ])
+    }
 }
 
